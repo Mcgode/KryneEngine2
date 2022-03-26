7ee973fcbd9cc813240d673b6a781738a0f7e9c5
Max Godefroy (Clyde) <max@godefroy.net>
2022-03-26 10:41:08 +0100

    Basic - Fixed app info

--- Examples/Basic/main.cpp
+++ Examples/Basic/main.cpp
@@ -5,6 +5,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     auto appInfo = KryneEngine::GraphicsCommon::ApplicationInfo();
+    appInfo.m_api = KryneEngine::GraphicsCommon::Api::Vulkan_1_2;
     KryneEngine::GraphicsContext graphicsContext(appInfo);
 
     while (graphicsContext.EndFrame());
