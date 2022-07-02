84871bc30528c04b190d0a87ff762c0da9da4b78
Max Godefroy (Clyde) <max@godefroy.net>
2022-07-02 10:50:03 +0200

    [Basic] Test FibersManager before graphics

--- Examples/Basic/main.cpp
+++ Examples/Basic/main.cpp
@@ -5,13 +5,13 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    auto appInfo = KryneEngine::GraphicsCommon::ApplicationInfo();
-    appInfo.m_api = KryneEngine::GraphicsCommon::Api::Vulkan_1_2;
-    KryneEngine::GraphicsContext graphicsContext(appInfo);
-
     {
         auto fibersManager = KryneEngine::FibersManager(6);
     }
+
+    auto appInfo = KryneEngine::GraphicsCommon::ApplicationInfo();
+    appInfo.m_api = KryneEngine::GraphicsCommon::Api::Vulkan_1_2;
+    KryneEngine::GraphicsContext graphicsContext(appInfo);
 
     while (graphicsContext.EndFrame());
 
