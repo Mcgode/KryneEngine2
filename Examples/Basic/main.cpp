7cea9f6e5be0f2d06340d1997493bbf1a6c62725
Max Godefroy (Clyde) <max@godefroy.net>
2022-07-03 15:19:25 +0200

    Test Fibers manager

--- Examples/Basic/main.cpp
+++ Examples/Basic/main.cpp
@@ -2,11 +2,41 @@
 #include <Graphics/Common/GraphicsContext.hpp>
 #include <Threads/FibersManager.hpp>
 
+std::atomic<KryneEngine::u32> counter = 0;
+
+static constexpr KryneEngine::u32 kCount = 100;
+KryneEngine::FiberJob counterJobs[kCount];
+
+void Job0(void* _data)
+{
+    counter++;
+}
+
+void Job1(void*)
+{
+    std::cout << "Counter value: " << counter << std::endl;
+
+    auto* fibersManager = KryneEngine::FibersManager::GetInstance();
+
+    auto syncCounter = fibersManager->InitAndBatchJobs(counterJobs, Job0, nullptr, kCount);
+
+    fibersManager->WaitForCounterAndReset(syncCounter);
+
+    std::cout << "Counter value: " << counter << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     {
         auto fibersManager = KryneEngine::FibersManager(6);
+
+        KryneEngine::FiberJob initJob;
+        auto syncCounter = fibersManager.InitAndBatchJobs(&initJob, Job1, nullptr);
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+
+        fibersManager.ResetCounter(syncCounter);
     }
 
     auto appInfo = KryneEngine::GraphicsCommon::ApplicationInfo();
