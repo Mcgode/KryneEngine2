400c34246ff738476f038b18969337af66da259d
Max Godefroy (Clyde) <max@godefroy.net>
2023-03-11 10:47:43 +0100

    [File] Implementation

---
+++ Lib/Memory/RangeMapping.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 10/03/2023.
+ */
+
+#pragma once
+
+#include <Common/KETypes.hpp>
+
+namespace KryneEngine
+{
+    struct MemoryRangeMapping
+    {
+        u64 m_size = 0;
+        u64 m_offset = 0;
+        u8* m_buffer = nullptr;
+    };
+}
