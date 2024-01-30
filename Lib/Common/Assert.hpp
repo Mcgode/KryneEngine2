a13223ef19a66cf111cb95791808f83a789ef472
Max Godefroy <max@godefroy.net>
2024-01-30 17:05:19 +0100

    [Assert] Fixed compilation

--- Lib/Common/Assert.hpp
+++ Lib/Common/Assert.hpp
@@ -11,11 +11,11 @@ namespace KryneEngine::Assertion
 	bool Error(const char* _function, u32 _line, const char* _file, const char* _formatMessage, ...);
 }
 
-#define KE_ASSERT_MSG(condition, message, args...) do \
+#define KE_ASSERT_MSG(condition, ...) do \
 	{ \
 		if (!(condition)) [[unlikely]] \
 		{\
-			if (KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), message, args)) \
+			if (KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__)) \
 			{ \
 				__debugbreak(); \
 			} \
@@ -24,13 +24,13 @@ namespace KryneEngine::Assertion
 	while(0)
 #define KE_ASSERT(condition) KE_ASSERT_MSG(condition, #condition)
 
-#define KE_VERIFY_MSG(condition, message, args...) ((condition) ? true: \
-	KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), message, args) ? __debugbreak(), false: false)
+#define KE_VERIFY_MSG(condition, ...) ((condition) ? true: \
+	KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__) ? __debugbreak(), false: false)
 #define KE_VERIFY(condition) KE_VERIFY_MSG(condition, #condition)
 
-#define KE_ERROR(message, args...) do { if (KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), message, args)) __debugbreak(); } while (0)
+#define KE_ERROR(...) do { if (KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__)) __debugbreak(); } while (0)
 
 #define IF_NOT_VERIFY(cond) if (!KE_VERIFY(cond)) [[unlikely]]
-#define IF_NOT_VERIFY_MSG(cond, message, args...) if (!KE_VERIFY_MSG(cond, message, args)) [[unlikely]]
+#define IF_NOT_VERIFY_MSG(cond, ...) if (!KE_VERIFY_MSG(cond, __VA_ARGS__)) [[unlikely]]
 #define VERIFY_OR_RETURN(cond, returnValue) IF_NOT_VERIFY(cond) return returnValue
 #define VERIFY_OR_RETURN_VOID(cond)  VERIFY_OR_RETURN(cond, )
