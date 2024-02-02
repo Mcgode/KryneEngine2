d24e69ae5f18e218b186cbd94de2ce228cced3b9
Max Godefroy <max@godefroy.net>
2024-02-02 13:07:06 +0100

    [Basic] Cleanup

--- Examples/Basic/main.cpp
+++ Examples/Basic/main.cpp
@@ -2,25 +2,26 @@
 #include <Graphics/Common/GraphicsContext.hpp>
 #include <Threads/FibersManager.hpp>
 #include <Threads/Semaphore.hpp>
-#include <Threads/RwMutex.hpp>
 
-std::atomic<KryneEngine::u32> counter = 0;
+using namespace KryneEngine;
 
-static constexpr KryneEngine::u32 kCount = 100;
-KryneEngine::FiberJob counterJobs[kCount];
-
-void Job0(void* _data)
+void Job0(void* _counterPtr)
 {
-    counter++;
+    (*static_cast<std::atomic<u32>*>(_counterPtr))++;
 }
 
 void Job1(void*)
 {
+    std::atomic<u32> counter = 0;
+
     std::cout << "Counter value: " << counter << std::endl;
 
-    auto* fibersManager = KryneEngine::FibersManager::GetInstance();
+    auto* fibersManager = FibersManager::GetInstance();
 
-    auto syncCounter = fibersManager->InitAndBatchJobs(counterJobs, Job0, nullptr, kCount);
+    static constexpr u32 kCount = 100;
+    FiberJob counterJobs[kCount];
+
+    const auto syncCounter = fibersManager->InitAndBatchJobs(counterJobs, Job0, &counter, kCount);
 
     fibersManager->WaitForCounterAndReset(syncCounter);
 
@@ -31,8 +32,8 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
 
     {
-        KryneEngine::BusySpinSemaphore semaphore(2);
-        KryneEngine::SpinLock lock;
+	    BusySpinSemaphore semaphore(2);
+	    SpinLock lock;
 
         lock.Lock();
 
@@ -65,25 +66,27 @@ int main() {
     }
 
     {
-        auto fibersManager = KryneEngine::FibersManager(6);
+        auto fibersManager = FibersManager(6);
 
-        KryneEngine::FiberJob initJob;
-        auto syncCounter = fibersManager.InitAndBatchJobs(&initJob, Job1, nullptr);
+        FiberJob initJob;
+        const auto syncCounter = fibersManager.InitAndBatchJobs(&initJob, Job1, nullptr);
 
         fibersManager.ResetCounter(syncCounter);
 
-        auto appInfo = KryneEngine::GraphicsCommon::ApplicationInfo();
+        auto appInfo = GraphicsCommon::ApplicationInfo();
         appInfo.m_applicationName = "Basic Example - Kryne Engine 2";
 #if defined(KE_GRAPHICS_API_VK)
-        appInfo.m_api = KryneEngine::GraphicsCommon::Api::Vulkan_1_2;
+        appInfo.m_api = GraphicsCommon::Api::Vulkan_1_3;
         appInfo.m_applicationName += " - Vulkan";
 #elif defined(KE_GRAPHICS_API_DX12)
-        appInfo.m_api = KryneEngine::GraphicsCommon::Api::DirectX12_1;
+        appInfo.m_api = KryneEngine::GraphicsCommon::Api::DirectX12_2;
         appInfo.m_applicationName += " - DirectX 12";
 #endif
-        KryneEngine::GraphicsContext graphicsContext(appInfo);
+        GraphicsContext graphicsContext(appInfo);
 
-        while (graphicsContext.EndFrame());
+        while (graphicsContext.EndFrame())
+        {
+        }
     }
 
     return 0;
