0ae24027d7d5874af7ed0cde089255b86d6b4644
Max Godefroy <max@godefroy.net>
2024-02-23 17:37:23 +0100

    [Assert] Added fatal assert

--- Lib/Common/Assert.hpp
+++ Lib/Common/Assert.hpp
@@ -24,11 +24,23 @@ namespace KryneEngine::Assertion
 	while(0)
 #define KE_ASSERT(condition) KE_ASSERT_MSG(condition, #condition)
 
+#define KE_ASSERT_FATAL_MSG(condition, ...) do \
+    { \
+        if (!(condition)) [[unlikely]] \
+        { \
+            KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__); \
+            throw std::runtime_error("Error was fatal"); \
+        } \
+    } \
+    while(0)
+#define KE_ASSERT_FATAL(condition) KE_ASSERT_FATAL_MSG(condition, #condition)
+
 #define KE_VERIFY_MSG(condition, ...) ((condition) ? true: \
 	KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__) ? __debugbreak(), false: false)
 #define KE_VERIFY(condition) KE_VERIFY_MSG(condition, #condition)
 
 #define KE_ERROR(...) do { if (KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__)) __debugbreak(); } while (0)
+#define KE_FATAL(...) do { KryneEngine::Assertion::Error(__builtin_FUNCTION(), __builtin_LINE(), __builtin_FILE(), __VA_ARGS__); throw std::runtime_error("Error was fatal"); } while (0)
 
 #define IF_NOT_VERIFY(cond) if (!KE_VERIFY(cond)) [[unlikely]]
 #define IF_NOT_VERIFY_MSG(cond, ...) if (!KE_VERIFY_MSG(cond, __VA_ARGS__)) [[unlikely]]
