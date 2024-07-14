6241c59f63feab537667c8209f34cef0e034837d
Max Godefroy (Bragg 2) <max@godefroy.net>
2024-07-14 13:17:07 +0200

    EASTL implementation fix

--- Core/Common/additional_impl.cpp
+++ Core/Common/additional_impl.cpp
@@ -7,10 +7,26 @@
 
 void* __cdecl operator new[](size_t size, const char* name, int flags, unsigned debugFlags, const char* file, int line)
 {
-    return new unsigned[size];
+    return new uint8_t[size];
 }
 
 void* __cdecl operator new[](size_t size, size_t alignment, size_t alignment_offset, const char* name, int flags, unsigned debugFlags, const char* file, int line)
 {
-    return new unsigned[size];
+    return new uint8_t[size];
+}
+
+void __cdecl operator delete(void* _p, const char* _name, int _flags, unsigned int _debugFlags, const char* _file, int _line)
+{
+  if (_p != nullptr)
+  {
+    ::operator delete(_p);
+  }
+}
+
+void __cdecl operator delete[](void* _p, const char* _name, int _flags, unsigned int _debugFlags, const char* _file, int _line)
+{
+  if (_p != nullptr)
+  {
+    ::operator delete[](_p);
+  }
 }
