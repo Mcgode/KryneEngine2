f3ebd1d188636baca1f0b1f229850da88b9df32a
Max Godefroy <max@godefroy.net>
2024-08-04 16:52:43 +0200

    [Modules][GraphicsUtils] Implemented DynamicBuffer

--- Modules/GraphicsUtils/DynamicBuffer.cpp
+++ Modules/GraphicsUtils/DynamicBuffer.cpp
@@ -5,13 +5,179 @@
  */
 
 #include "DynamicBuffer.hpp"
+#include <Graphics/Common/GraphicsContext.hpp>
 
-namespace KryneEngine
+namespace KryneEngine::Modules::GraphicsUtils
 {
-    namespace Modules
+    void DynamicBuffer::Init(GraphicsContext& _graphicsContext, const BufferCreateDesc& _bufferDesc, u8 _frameCount)
     {
-        namespace GraphicsUtils
+        KE_ASSERT_MSG(
+            (_bufferDesc.m_usage & MemoryUsage::USAGE_TYPE_MASK) == MemoryUsage::StageEveryFrame_UsageType,
+            "Buffer usage type should be `StageEveryFrame_UsageType`");
+
+        m_mappableBuffers.Resize(_frameCount);
+        BufferHandle baseBuffer = _graphicsContext.CreateBuffer(_bufferDesc);
+
+        if (_graphicsContext.NeedsStagingBuffer(baseBuffer))
         {
-        } // namespace GraphicsUtils
-    } // namespace Modules
-} // namespace KryneEngine
+            // Must go through staging buffers
+
+            m_gpuBuffer = baseBuffer;
+            m_gpuRecreateDesc = _bufferDesc;
+
+            m_mappableRecreateDesc = {
+                .m_desc = _bufferDesc.m_desc,
+                .m_usage = MemoryUsage::StageOnce_UsageType | MemoryUsage::TransferSrcBuffer,
+            };
+            for (u8 i = 0; i < _frameCount; i++)
+            {
+                m_mappableBuffers[i] = _graphicsContext.CreateBuffer(m_mappableRecreateDesc);
+            }
+        }
+        else
+        {
+            // Will be able to use cpu-writable directly on the GPU
+
+            m_mappableBuffers[0] = baseBuffer;
+            for (u8 i = 1; i < _frameCount; i++)
+            {
+                m_mappableBuffers[i] = _graphicsContext.CreateBuffer(m_mappableRecreateDesc);
+            }
+        }
+
+        m_sizes.Resize(_frameCount);
+        m_sizes.InitAll(m_mappableRecreateDesc.m_desc.m_size);
+    }
+
+    void DynamicBuffer::RequestResize(u64 _size)
+    {
+        m_mappableRecreateDesc.m_desc.m_size = _size;
+        if (m_gpuBuffer != GenPool::kInvalidHandle)
+        {
+            m_gpuRecreateDesc.m_desc.m_size = _size;
+        }
+    }
+
+    void* DynamicBuffer::Map(GraphicsContext& _graphicsContext, u8 _frameIndex)
+    {
+        if (!m_gpuBuffersToFree.empty() && m_gpuBuffersToFree.front().m_atIndex == _frameIndex)
+        {
+            _graphicsContext.DestroyBuffer(m_gpuBuffersToFree.front().m_buffer);
+            m_gpuBuffersToFree.erase(m_gpuBuffersToFree.begin());
+        }
+
+        if (m_mappableRecreateDesc.m_desc.m_size != m_sizes[_frameIndex])
+        {
+            _graphicsContext.DestroyBuffer(m_mappableBuffers[_frameIndex]);
+            m_mappableBuffers[_frameIndex] = _graphicsContext.CreateBuffer(m_mappableRecreateDesc);
+
+            if (m_gpuBuffer != GenPool::kInvalidHandle)
+            {
+                const u8 frameCount = m_mappableBuffers.Size();
+                m_gpuBuffersToFree.push_back(BufferToFree {
+                    m_gpuBuffer,
+                    static_cast<u8>((_frameIndex + frameCount - 1u) % frameCount),
+                });
+                m_gpuBuffer = _graphicsContext.CreateBuffer(m_gpuRecreateDesc);
+            }
+
+            m_sizes[_frameIndex] = m_mappableRecreateDesc.m_desc.m_size;
+        }
+
+        m_currentMapping.m_buffer = m_mappableBuffers[_frameIndex];
+        m_currentMapping.m_size = m_sizes[_frameIndex];
+        _graphicsContext.MapBuffer(m_currentMapping);
+        return m_currentMapping.m_ptr;
+    }
+
+    void DynamicBuffer::Unmap(GraphicsContext& _graphicsContext) {
+        _graphicsContext.UnmapBuffer(m_currentMapping);
+    }
+
+    void DynamicBuffer::PrepareBuffers(
+        GraphicsContext& _graphicsContext,
+        CommandList _commandLine,
+        BarrierAccessFlags _accessFlags,
+        u8 _frameIndex)
+    {
+        if (m_gpuBuffer == GenPool::kInvalidHandle)
+        {
+            BufferMemoryBarrier memoryBarrier {
+                .m_stagesSrc = BarrierSyncStageFlags::None,
+                .m_stagesDst = BarrierSyncStageFlags::All,
+                .m_accessSrc = BarrierAccessFlags::All,
+                .m_accessDst = _accessFlags,
+                .m_buffer = m_mappableBuffers[_frameIndex],
+            };
+
+            _graphicsContext.PlaceMemoryBarriers(
+                _commandLine,
+                {},
+                { &memoryBarrier, 1 },
+                {});
+        }
+        else
+        {
+            const BufferCopyParameters params {
+                .m_copySize = m_sizes[_frameIndex],
+                .m_bufferSrc = m_mappableBuffers[_frameIndex],
+                .m_bufferDst = m_gpuBuffer,
+            };
+
+            {
+                BufferMemoryBarrier memoryBarriers[2] = {
+                    {
+                        .m_stagesSrc = BarrierSyncStageFlags::None,
+                        .m_stagesDst = BarrierSyncStageFlags::Transfer,
+                        .m_accessSrc = BarrierAccessFlags::All,
+                        .m_accessDst = BarrierAccessFlags::TransferSrc,
+                        .m_buffer = params.m_bufferSrc,
+                    },
+                    {
+                        .m_stagesSrc = BarrierSyncStageFlags::None,
+                        .m_stagesDst = BarrierSyncStageFlags::Transfer,
+                        .m_accessSrc = BarrierAccessFlags::All,
+                        .m_accessDst = BarrierAccessFlags::TransferDst,
+                        .m_buffer = params.m_bufferDst,
+                    }
+                };
+
+                _graphicsContext.PlaceMemoryBarriers(
+                    _commandLine,
+                    {},
+                    { memoryBarriers, 2 },
+                    {});
+            }
+
+            _graphicsContext.CopyBuffer(_commandLine, params);
+
+            {
+                BufferMemoryBarrier memoryBarrier {
+                    .m_stagesSrc = BarrierSyncStageFlags::Transfer,
+                    .m_stagesDst = BarrierSyncStageFlags::All,
+                    .m_accessSrc = BarrierAccessFlags::TransferDst,
+                    .m_accessDst = _accessFlags,
+                    .m_buffer = params.m_bufferDst,
+                };
+
+                _graphicsContext.PlaceMemoryBarriers(
+                    _commandLine,
+                    {},
+                    { &memoryBarrier, 1 },
+                    {});
+            }
+        }
+    }
+
+    void DynamicBuffer::Destroy(GraphicsContext& _graphicsContext)
+    {
+        for (auto buffer: m_mappableBuffers)
+        {
+            _graphicsContext.DestroyBuffer(buffer);
+        }
+        if (m_gpuBuffer != GenPool::kInvalidHandle)
+        {
+            _graphicsContext.DestroyBuffer(m_gpuBuffer);
+        }
+    }
+}
--- Modules/GraphicsUtils/DynamicBuffer.hpp
+++ Modules/GraphicsUtils/DynamicBuffer.hpp
@@ -6,9 +6,45 @@
 
 #pragma once
 
+#include <Graphics/Common/Buffer.hpp>
+#include <Graphics/Common/Handles.hpp>
+#include <Graphics/Common/GraphicsContext.hpp>
+
 namespace KryneEngine::Modules::GraphicsUtils
 {
     class DynamicBuffer
     {
+    public:
+        void Init(GraphicsContext& _graphicsContext, const BufferCreateDesc& _bufferDesc, u8 _frameCount);
+        void RequestResize(u64 _size);
+        void* Map(GraphicsContext& _graphicsContext, u8 _frameIndex);
+        void Unmap(GraphicsContext& _graphicsContext);
+        void PrepareBuffers(
+            GraphicsContext& _graphicsContext,
+            CommandList _commandLine,
+            KryneEngine::BarrierAccessFlags _accessFlags,
+            u8 _frameIndex);
+
+        [[nodiscard]] u64 GetSize(u8 _frameIndex) const
+        {
+            return m_sizes[_frameIndex];
+        }
+
+        void Destroy(GraphicsContext& _graphicsContext);
+
+    private:
+        BufferCreateDesc m_mappableRecreateDesc;
+        BufferCreateDesc m_gpuRecreateDesc;
+        DynamicArray<BufferHandle> m_mappableBuffers;
+        DynamicArray<u64> m_sizes;
+        BufferHandle m_gpuBuffer { GenPool::kInvalidHandle };
+        BufferMapping m_currentMapping { { GenPool::kInvalidHandle } };
+
+        struct BufferToFree
+        {
+            BufferHandle m_buffer;
+            u8 m_atIndex = 0;
+        };
+        eastl::vector<BufferToFree> m_gpuBuffersToFree;
     };
 }
