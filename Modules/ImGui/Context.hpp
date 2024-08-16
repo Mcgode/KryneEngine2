7cbbdca2dab36781e3fe629fe068a49e48bc0e91
Max Godefroy <max@godefroy.net>
2024-08-16 18:22:05 +0200

    [Modules][ImGui] Add detailed documentation to ImGui Context class
    Update the ImGui Context class by adding comprehensive comments for constructors, methods, and class description. This includes explanations for parameters and functionality aimed at improving code readability and maintainability.

--- Modules/ImGui/Context.hpp
+++ Modules/ImGui/Context.hpp
@@ -17,18 +17,59 @@ namespace KryneEngine
 
 namespace KryneEngine::Modules::ImGui
 {
-
+    /**
+     * @class Context
+     *
+     * This class represents the rendering and input handling context for ImGui (Immediate Mode GUI).
+     */
     class Context
     {
     public:
+        /**
+         * @brief Constructs a Context object.
+         *
+         * This constructor initializes the ImGui context by calling ImGui::CreateContext().
+         * It also sets up the ImGuiIO data structure and initializes the vertex and index dynamic buffers.
+         *
+         * @param _window The Window object associated with the Context.
+         * @param _renderPass The RenderPassHandle object used for building the ImGui PSO.
+         */
         explicit Context(Window* _window, RenderPassHandle _renderPass);
+
         ~Context();
 
+        /**
+         * @brief Shut down the context.
+         *
+         * @param _graphicsContext The graphics context that owns the graphic resources.
+         */
         void Shutdown(GraphicsContext* _graphicsContext);
 
+        /**
+         * Sets up the ImGui context for a new frame.
+         * Updates input and window data.
+         *
+         * @param _window The Window object.
+         * @param _commandList The command list.
+         */
         void NewFrame(Window* _window, CommandList _commandList);
 
+        /**
+         * @brief Prepares the rendering context for a new frame by updating the vertex and index buffers.
+         *
+         * @param _graphicsContext The graphics context used for rendering.
+         * @param _commandList The command list used for uploading the buffers.
+         */
         void PrepareToRenderFrame(GraphicsContext* _graphicsContext, CommandList _commandList);
+
+        /**
+         * @brief Renders a frame using the provided graphics context and command list.
+         *
+         * This function is responsible for rendering the ImGui UI for a single frame.
+         *
+         * @param _graphicsContext The graphics context used for rendering.
+         * @param _commandList The command list used for rendering.
+         */
         void RenderFrame(GraphicsContext* _graphicsContext, CommandList _commandList);
 
     private:
