846d5b41ba8cda8e6010bb525e1e961d69f64356
Max Godefroy <max@godefroy.net>
2024-08-17 13:00:15 +0200

    [Window][Input] Add window focus event callbacks
    Introduce registration and unregistration for window focus event callbacks. Implement a mutex-protected mechanism to manage and execute these callbacks in response to window focus changes.

--- Core/Window/Window.cpp
+++ Core/Window/Window.cpp
@@ -32,6 +32,8 @@ namespace KryneEngine
         m_graphicsContext = eastl::make_unique<GraphicsContext>(_appInfo, this);
 
         m_inputManager = eastl::make_unique<InputManager>(this);
+
+        glfwSetWindowFocusCallback(m_glfwWindow, WindowFocusCallback);
     }
 
     Window::~Window()
@@ -47,6 +49,33 @@ namespace KryneEngine
         glfwPollEvents();
 
         return !glfwWindowShouldClose(m_glfwWindow);
+    }
+
+    u32 Window::RegisterWindowFocusEventCallback(eastl::function<void(bool)>&& _callback)
+    {
+        const auto lock = m_callbackMutex.AutoLock();
+
+        const u32 id = m_windowFocusEventCounter++;
+        m_windowFocusEventListeners.emplace(id, _callback);
+        return id;
+    }
+
+    void Window::UnregisterWindowFocusEventCallback(u32 _id)
+    {
+        const auto lock = m_callbackMutex.AutoLock();
+        m_windowFocusEventListeners.erase(_id);
+    }
+
+    void Window::WindowFocusCallback(GLFWwindow* _window, s32 _focused)
+    {
+        auto* window = static_cast<Window*>(glfwGetWindowUserPointer(_window));
+
+        const auto lock = window->m_callbackMutex.AutoLock();
+
+        for (const auto& pair : window->m_windowFocusEventListeners)
+        {
+            pair.second(_focused);
+        }
     }
 }
 
