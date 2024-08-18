60b0cb4a4ed04e653a8a216a996b37288c8ce66e
Max Godefroy <max@godefroy.net>
2024-08-18 21:23:16 +0200

    [Modules][RpsRuntime] Set up placeholder backend interface functions
    Added multiple backend interface functions to ensure compliance with the base class interface. All new functions currently return a not implemented error, and error logging has been added for certain methods. This sets the foundation for future implementations of the backend's resource management and command recording capabilities.

--- Modules/RpsRuntime/Private/Backend.cpp
+++ Modules/RpsRuntime/Private/Backend.cpp
@@ -15,17 +15,81 @@ namespace KryneEngine::Modules::RpsRuntime
         , m_device(_device)
     {}
 
-    RpsResult Backend::CreateHeaps(const rps::RenderGraphUpdateContext& _context, rps::ArrayRef<rps::HeapInfo> _heaps)
+    RpsResult Backend::RecordCommands(
+        const rps::RenderGraph& renderGraph, const RpsRenderGraphRecordCommandInfo& recordInfo) const
     {
-        return RuntimeBackend::CreateHeaps(_context, _heaps);
+        return RPS_ERROR_NOT_IMPLEMENTED;
     }
 
-    RpsResult Backend::RecordCommands(
-        const rps::RenderGraph& _renderGraph, const RpsRenderGraphRecordCommandInfo& _recordInfo) const
+    RpsResult Backend::RecordCmdRenderPassBegin(const rps::RuntimeCmdCallbackContext& context) const
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    RpsResult Backend::RecordCmdRenderPassEnd(const rps::RuntimeCmdCallbackContext& context) const
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    RpsResult Backend::RecordCmdFixedFunctionBindingsAndDynamicStates(const rps::RuntimeCmdCallbackContext& context) const
     {
         return RPS_ERROR_NOT_IMPLEMENTED;
     }
 
     void Backend::DestroyRuntimeResourceDeferred(rps::ResourceInstance& resource)
-    {}
+    {
+        KE_ERROR("Not implemented");
+    }
+
+    RpsResult Backend::UpdateFrame(const rps::RenderGraphUpdateContext& context)
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    RpsResult Backend::CreateHeaps(const rps::RenderGraphUpdateContext& context, rps::ArrayRef<rps::HeapInfo> heaps)
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    void Backend::DestroyHeaps(rps::ArrayRef<rps::HeapInfo> heaps)
+    {
+        KE_ERROR("Not implemented");
+        RuntimeBackend::DestroyHeaps(heaps);
+    }
+
+    RpsResult Backend::CreateResources(const rps::RenderGraphUpdateContext& context, rps::ArrayRef<rps::ResourceInstance> resources)
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    void Backend::DestroyResources(rps::ArrayRef<rps::ResourceInstance> resources)
+    {
+        KE_ERROR("Not implemented");
+        RuntimeBackend::DestroyResources(resources);
+    }
+
+    RpsResult Backend::CreateCommandResources(const rps::RenderGraphUpdateContext& context)
+    {
+        return RPS_ERROR_NOT_IMPLEMENTED;
+    }
+
+    void Backend::DestroyCommandResources()
+    {
+        KE_ERROR("Not implemented");
+        RuntimeBackend::DestroyCommandResources();
+    }
+
+    void Backend::RecordDebugMarker(
+        const rps::RuntimeCmdCallbackContext& context,
+        RpsRuntimeDebugMarkerMode mode, rps::StrRef name) const
+    {
+        KE_ERROR("Not implemented");
+        RuntimeBackend::RecordDebugMarker(context, mode, name);
+    }
+
+    bool Backend::ShouldResetAliasedResourcesPrevFinalAccess() const
+    {
+        KE_ERROR("Not implemented");
+        return RuntimeBackend::ShouldResetAliasedResourcesPrevFinalAccess();
+    }
 } // namespace KryneEngine::Modules::RpsRuntime
--- Modules/RpsRuntime/Private/Backend.hpp
+++ Modules/RpsRuntime/Private/Backend.hpp
@@ -17,15 +17,39 @@ namespace KryneEngine::Modules::RpsRuntime
     public:
         explicit Backend(Device& _device, rps::RenderGraph& _renderGraph);
 
-        RpsResult CreateHeaps(
-            const rps::RenderGraphUpdateContext& _context,
-            rps::ArrayRef<rps::HeapInfo> _heaps) final;
-
         [[nodiscard]] RpsResult RecordCommands(
-            const rps::RenderGraph& _renderGraph,
-            const RpsRenderGraphRecordCommandInfo& _recordInfo) const final;
+            const rps::RenderGraph& renderGraph, const RpsRenderGraphRecordCommandInfo& recordInfo) const override;
 
-        void DestroyRuntimeResourceDeferred(rps::ResourceInstance& resource) final;
+        [[nodiscard]] RpsResult RecordCmdRenderPassBegin(const rps::RuntimeCmdCallbackContext& context) const override;
+        [[nodiscard]] RpsResult RecordCmdRenderPassEnd(const rps::RuntimeCmdCallbackContext& context) const override;
+
+        [[nodiscard]] RpsResult RecordCmdFixedFunctionBindingsAndDynamicStates(
+            const rps::RuntimeCmdCallbackContext& context) const override;
+
+        void DestroyRuntimeResourceDeferred(rps::ResourceInstance& resource) override;
+
+    protected:
+        RpsResult UpdateFrame(const rps::RenderGraphUpdateContext& context) override;
+
+        RpsResult CreateHeaps(
+            const rps::RenderGraphUpdateContext& context,
+            rps::ArrayRef<rps::HeapInfo> heaps) override;
+        void DestroyHeaps(rps::ArrayRef<rps::HeapInfo> heaps) override;
+
+        RpsResult CreateResources(
+            const rps::RenderGraphUpdateContext& context,
+            rps::ArrayRef<rps::ResourceInstance> resources) override;
+        void DestroyResources(rps::ArrayRef<rps::ResourceInstance> resources) override;
+
+        RpsResult CreateCommandResources(const rps::RenderGraphUpdateContext& context) override;
+        void DestroyCommandResources() override;
+
+        void RecordDebugMarker(
+            const rps::RuntimeCmdCallbackContext& context,
+            RpsRuntimeDebugMarkerMode mode,
+            rps::StrRef name) const override;
+
+        bool ShouldResetAliasedResourcesPrevFinalAccess() const override;
 
     private:
         Device& m_device;
