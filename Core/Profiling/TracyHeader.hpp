c63673040aac97ef04fcd1b1a8040a44a5ed9949
Max Godefroy <max@godefroy.net>
2024-08-18 11:05:41 +0200

    [Profiling] Define default profiling colors in TracyHeader.hpp
    Added default colors for profiling zones and functions in TracyHeader.hpp to enhance code readability and maintainability. This change ensures that if specific colors are not set, the default colors will be used, providing a consistent visual representation.

--- Core/Profiling/TracyHeader.hpp
+++ Core/Profiling/TracyHeader.hpp
@@ -8,6 +8,17 @@
 
 #include <tracy/Tracy.hpp>
 
+#define KE_TRACY_DEFAULT_COLOR 0x0F4B80
+#define KE_TRACY_DEFAULT_FUNC_COLOR 0x1876C9
+
+#if !defined(KE_TRACY_COLOR)
+#   define KE_TRACY_COLOR KE_TRACY_DEFAULT_COLOR
+#endif
+
+#if !defined(KE_TRACY_FUNC_COLOR)
+#   define KE_TRACY_FUNC_COLOR KE_TRACY_DEFAULT_FUNC_COLOR
+#endif
+
 #define KE_ZoneScopedFunction(name) ZoneScopedNC(name, KE_TRACY_FUNC_COLOR)
 #define KE_ZoneScoped(name) ZoneScopedNC(name, KE_TRACY_COLOR)
 
