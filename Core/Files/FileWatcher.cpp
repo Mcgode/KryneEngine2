c3fc0447107affbcf63a83153d07f18c086446f0
maxgodefroy <max@godefroy.net>
2024-10-22 15:42:00 +0200

    [Core][Files] Implemented macOS support for file watcher, using kqueue

--- Core/Files/FileWatcher.cpp
+++ Core/Files/FileWatcher.cpp
@@ -10,6 +10,12 @@
 #	include <Platform/Windows.h>
 #endif
 
+#if defined(__APPLE__)
+#   include <sys/event.h>
+#   include <sys/fcntl.h>
+#endif
+
+
 #include <Common/Assert.hpp>
 
 namespace KryneEngine
@@ -30,6 +36,11 @@ namespace KryneEngine
 
         m_watcherThread = std::thread([&, this]()
         {
+            if (m_watchedDirectories.empty())
+            {
+                return;
+            }
+
 #if defined(_WIN32)
             const DWORD filters = FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_DIR_NAME;
 
@@ -91,6 +102,72 @@ namespace KryneEngine
             {
                 FindCloseChangeNotification(handle);
             }
+#elif (__APPLE__)
+            const s32 kq = kqueue();
+            IF_NOT_VERIFY_MSG(kq != -1, "Unable to create kqueue for file watcher")
+            {
+                return;
+            }
+
+            const DynamicArray<s32> dirFDs(m_watchedDirectories.size());
+
+            for (u32 i = 0; i < m_watchedDirectories.size(); i++)
+            {
+                const auto& directory = m_watchedDirectories[i];
+                dirFDs[i] = open(directory.GetPath().c_str(), O_RDONLY);
+                IF_NOT_VERIFY_MSG(dirFDs[i] != -1, "Unable to open dir descriptor for '%s'", directory.GetPath().c_str())
+                {
+                    return;
+                }
+
+                struct kevent event = {};
+                EV_SET(
+                    &event,
+                    dirFDs[i],
+                    EVFILT_VNODE,
+                    EV_ADD | EV_ENABLE | EV_CLEAR,
+                    NOTE_WRITE | NOTE_EXTEND | NOTE_ATTRIB | NOTE_LINK | NOTE_RENAME | NOTE_DELETE,
+                    0,
+                    reinterpret_cast<void*>(i));
+
+                if (kevent(kq, &event, 1, nullptr, 0, nullptr) == -1)
+                {
+                    KE_ERROR("Failed to add directory to file watcher: %s", directory.GetPath().c_str());
+                    close(dirFDs[i]);
+                    dirFDs[i] = -1;
+                }
+            }
+
+            do
+            {
+                struct kevent events[16];
+                constexpr timespec timeout = { .tv_sec = 1, .tv_nsec = 0 };
+                const s32 waitResult = kevent(kq, nullptr, 0, events, 16, &timeout);
+
+                IF_NOT_VERIFY_MSG(waitResult != -1, "Unable to get kevent for file watcher")
+                {
+                    return;
+                }
+
+                for (s32 i = 0; i < waitResult; i++)
+                {
+                    if (events[i].filter == EVFILT_VNODE)
+                    {
+                        const u32 index = reinterpret_cast<intptr_t>(events[i].udata);
+                        m_watchedDirectories[index].Update();
+                    }
+                }
+
+                for (const s32 dirFD: dirFDs)
+                {
+                    if (dirFD != -1)
+                    {
+                        close(dirFD);
+                    }
+                }
+                close(kq);
+            }
+            while (!m_shouldStop);
 #else
 #error Unsupported
 #endif
