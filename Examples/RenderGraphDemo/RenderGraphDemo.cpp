70699cb69de94ab727af824f6f05d3775c05e733
maxgodefroy <max@godefroy.net>
2024-11-13 18:14:09 +0100

    [Examples][RenderGraphDemo] Placeholder setup

---
+++ Examples/RenderGraphDemo/RenderGraphDemo.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 13/11/2024.
+ */
+
+#include <RenderGraph/Builder.hpp>
+
+using namespace KryneEngine::Modules;
+
+int main()
+{
+    RenderGraph::Builder builder {};
+
+    return 0;
+}
