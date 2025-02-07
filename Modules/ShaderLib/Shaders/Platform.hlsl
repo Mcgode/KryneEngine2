4a9e3c8e965438de8e563738dcb785b225b6c549
maxgodefroy <max@godefroy.net>
2025-02-07 20:24:45 +0100

    Experiment with slang library

--- Modules/ShaderLib/Shaders/Platform.hlsl
+++ Modules/ShaderLib/Shaders/Platform.hlsl
@@ -1,10 +1,4 @@
 
-#ifdef __spirv__
-#   define vkPushConstant [[vk::push_constant]]
-#   define vkLocation(index) [[vk::location(index)]]
-#   define vkBinding(index, set) [[vk::binding(index, set)]]
-#else
-#   define vkPushConstant
-#   define vkLocation(index)
-#   define vkBinding(index, set)
-#endif
+#define vkPushConstant [[vk::push_constant]]
+#define vkLocation(index) [[vk::location(index)]]
+#define vkBinding(index, set) [[vk::binding(index, set)]]
