9fb00b10a3a40fa060dfb2ac828dc04e3e6c25c5
maxgodefroy <max@godefroy.net>
2025-03-15 07:47:54 +0100

    [Core][Math] Update Vector4Base constructor template parameter
    Replaced 'size_t A' with 'bool S' in the Vector4Base constructor to better align with the type of the associated Vector3Base template parameter. This enhances consistency and resolves potential mismatches in template instantiation.

--- Core/Include/KryneEngine/Core/Math/Vector4.hpp
+++ Core/Include/KryneEngine/Core/Math/Vector4.hpp
@@ -39,9 +39,9 @@ namespace KryneEngine::Math
         requires std::is_constructible_v<T, U>
         explicit Vector4Base(const Vector4Base<U, S> &_other) : Vector4Base(_other.x, _other.y, _other.z, _other.w) {}
 
-        template <typename U0, typename U1, size_t A>
+        template <typename U0, typename U1, bool S>
         requires std::is_constructible_v<T, U0> && std::is_constructible_v<T, U1>
-        explicit Vector4Base(const Vector3Base<U0, A>& _vec3, U1 _w = 0): Vector4Base(_vec3.x, _vec3.y, _vec3.z, _w) {}
+        explicit Vector4Base(const Vector3Base<U0, S>& _vec3, U1 _w = 0): Vector4Base(_vec3.x, _vec3.y, _vec3.z, _w) {}
 
         template <typename U0, typename U1, typename U2, size_t A>
         requires std::is_constructible_v<T, U0> && std::is_constructible_v<T, U1> && std::is_constructible_v<T, U2>
