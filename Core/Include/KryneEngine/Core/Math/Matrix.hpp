8dfb5a3aff96118d240ab76ef492d26be420fcea
maxgodefroy <max@godefroy.net>
2025-03-21 09:34:31 +0100

    [Core][Math] Add Matrix44 and expand matrix type aliases
    Introduced Matrix44-based types for both float and double, with SIMD and non-SIMD variations. Updated Matrix33 aliases to include SIMD variants for better flexibility and precision control. This enhances the matrix math capabilities in the engine.

--- Core/Include/KryneEngine/Core/Math/Matrix.hpp
+++ Core/Include/KryneEngine/Core/Math/Matrix.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "KryneEngine/Core/Math/Matrix33.hpp"
+#include "KryneEngine/Core/Math/Matrix44.hpp"
 
 #ifndef KE_DEFAULT_MATRIX_ROW_MAJOR
 #   define KE_DEFAULT_MATRIX_ROW_MAJOR 1
@@ -14,5 +15,13 @@
 
 namespace KryneEngine
 {
-    using float3x3 = Math::Matrix33Base<float, sizeof(float), KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using float3x3 = Math::Matrix33Base<float, false, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using double3x3 = Math::Matrix33Base<double, false, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using float3x3_simd = Math::Matrix33Base<float, true, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using double3x3_simd = Math::Matrix33Base<double, true, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+
+    using float4x4 = Math::Matrix44Base<float, false, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using double4x4 = Math::Matrix44Base<double, false, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using float4x4_simd = Math::Matrix44Base<float, true, KE_DEFAULT_MATRIX_ROW_MAJOR>;
+    using double4x4_simd = Math::Matrix44Base<double, true, KE_DEFAULT_MATRIX_ROW_MAJOR>;
 }
