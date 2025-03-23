8a99dc706676ebc7831e90f49cad1b2feb0d8c46
maxgodefroy <max@godefroy.net>
2025-03-23 20:06:59 +0100

    [Core][Math] Fixed matrix type

--- Core/Include/KryneEngine/Core/Math/Transform.hpp
+++ Core/Include/KryneEngine/Core/Math/Transform.hpp
@@ -16,7 +16,7 @@ namespace KryneEngine::Math
     template<Matrix44Type Mat44, Vector3Type Vec3, QuaternionType Quat>
     Mat44 ComputeTransformMatrix(const Vec3& _position, const Quat& _rotation, const Vec3& _scale)
     {
-        auto r = ToMatrix33<typename Mat44::ScalarType, Mat44::kSimdOptimal, Mat44::kRowMajorLayout>(_rotation);
+        auto r = ToMatrix33<Matrix33Base<typename Mat44::ScalarType, false, Mat44::kRowMajorLayout>>(_rotation);
         return Mat44 {
             r.Get(0, 0) * _scale.x, r.Get(0, 1) * _scale.y, r.Get(0, 2) * _scale.z, _position.x,
             r.Get(1, 0) * _scale.x, r.Get(1, 1) * _scale.y, r.Get(1, 2) * _scale.z, _position.y,
