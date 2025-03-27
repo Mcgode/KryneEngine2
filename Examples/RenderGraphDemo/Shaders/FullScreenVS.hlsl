e6c00081ada9b7b58d8ca6d40648f7108d2f0bd7
Max Godefroy <max@godefroy.net>
2025-03-27 22:30:31 +0100

    [Samples][RenderGraphDemo] Add FullScreenVS vertex shader
    Introduced a new vertex shader file and its configuration for the RenderGraphDemo example. This shader computes fullscreen triangle vertex data with a depth value passed via push constants.

---
+++ Examples/RenderGraphDemo/Shaders/FullScreenVS.hlsl
@@ -0,0 +1,39 @@
+/**
+* @file
+ * @author Max Godefroy
+ * @date 27/03/2025.
+ */
+
+#include "Platform.hlsl"
+
+struct VsInput
+{
+    uint vertexIndex : SV_VertexID;
+};
+
+struct VsOutput
+{
+    float4 position : SV_POSITION;
+};
+
+struct Parameters
+{
+    float m_depth;
+};
+
+vkPushConstant
+ConstantBuffer<Parameters> parameters;
+
+VsOutput main(VsInput input)
+{
+    VsOutput output;
+
+    output.position = float4(
+        input.vertexIndex == 1 ? 3.f : -1.f,
+        input.vertexIndex == 2 ? 3.f : -1.f,
+        parameters.m_depth,
+        1.f
+    );
+
+    return output;
+}
