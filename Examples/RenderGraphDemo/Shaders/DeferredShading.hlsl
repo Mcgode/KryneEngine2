1b21d513698626d5077baf8cc78548f13afbac7d
maxgodefroy <max@godefroy.net>
2025-04-10 21:49:30 +0200

    [Samples][RenderGraphDemo] Fix lighting direction in deferred shading computation
    The sun light direction was incorrectly used in the dot product, causing lighting to behave incorrectly. This fix inverts the direction to ensure proper lighting calculations in the shader.

--- Examples/RenderGraphDemo/Shaders/DeferredShading.hlsl
+++ Examples/RenderGraphDemo/Shaders/DeferredShading.hlsl
@@ -57,8 +57,8 @@ FsOutput DeferredShadingMain(const in FsInput _input)
     const float3 normalW = gBufferNormal.Load(int3(pixelCoords, 0)).rgb * 2.f - 1.f;
     const float3 albedo = gBufferAlbedo.Load(int3(pixelCoords, 0)).rgb;
 
-    float3 directLighting = saturate(dot(normalW, frameData.m_sunLightDirection)) * frameData.m_sunDiffuse;
     directLighting *= deferredShadows.Load(int3(pixelCoords, 0)).rrr;
+    float3 directLighting = saturate(dot(normalW, -frameData.m_sunLightDirection)) * frameData.m_sunDiffuse;
 
     const float3 diffuse = albedo * (directLighting + gBufferLight.Load(int3(pixelCoords, 0)).rgb);
 
