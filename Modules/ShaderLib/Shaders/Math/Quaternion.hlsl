dd4a08defceace4c60d1fe76df993e39dba036ff
maxgodefroy <max@godefroy.net>
2025-04-10 21:48:42 +0200

    [Samples][RenderGraphDemo] Document quaternion application to vector

--- Modules/ShaderLib/Shaders/Math/Quaternion.hlsl
+++ Modules/ShaderLib/Shaders/Math/Quaternion.hlsl
@@ -8,6 +8,11 @@ namespace Quaternion
 {
     float3 Apply(const in float4 _quaternion, const in float3 _vector)
     {
+        // Based on https://blog.molecular-matters.com/2013/05/24/a-faster-quaternion-vector-multiplication/
+
+        // const float3 t = 2 * cross(_quaternion.xyz, _vector);
+        // return _vector + _quaternion.w * t + cross(_quaternion.xyz, t);
+
         return 2.f * _quaternion.xyz * dot(_vector, _quaternion.xyz)
             + _vector * (_quaternion.w * _quaternion.w - dot(_quaternion.xyz, _quaternion.xyz))
             + cross(_quaternion.xyz, _vector) * 2.f * _quaternion.w;
