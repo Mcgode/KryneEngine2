86a2c201f9971b56fa04efb8a4705da8ea78ce37
Max Godefroy <max@godefroy.net>
2025-04-11 17:03:20 +0200

    [Samples][RenderGraphDemo] Fix barrier stage flags in TorusKnot buffer transitions.
    Adjusted pipeline stage flags for index and vertex buffers to use more precise `IndexInputAssembly` and `VertexInputAssembly` stages. This ensures proper synchronization and improves clarity in buffer usage.

--- Examples/RenderGraphDemo/Scene/TorusKnot.cpp
+++ Examples/RenderGraphDemo/Scene/TorusKnot.cpp
@@ -251,14 +251,14 @@ namespace KryneEngine::Samples::RenderGraphDemo
         const BufferMemoryBarrier postCopyBarriers[] = {
             {
                 .m_stagesSrc = BarrierSyncStageFlags::Transfer,
-                .m_stagesDst = BarrierSyncStageFlags::InputAssembly,
+                .m_stagesDst = BarrierSyncStageFlags::IndexInputAssembly,
                 .m_accessSrc = BarrierAccessFlags::TransferSrc,
                 .m_accessDst = BarrierAccessFlags::IndexBuffer,
                 .m_buffer = m_indexBuffer,
             },
             {
                 .m_stagesSrc = BarrierSyncStageFlags::Transfer,
-                .m_stagesDst = BarrierSyncStageFlags::InputAssembly,
+                .m_stagesDst = BarrierSyncStageFlags::VertexInputAssembly,
                 .m_accessSrc = BarrierAccessFlags::TransferSrc,
                 .m_accessDst = BarrierAccessFlags::VertexBuffer,
                 .m_buffer = m_vertexBuffer,
