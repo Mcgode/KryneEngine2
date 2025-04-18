33dce852485cfed5bc63f6e8387ef7c6cb65725b
maxgodefroy <max@godefroy.net>
2025-04-18 22:14:58 +0200

    [Modules][ShaderReflection] CreateBlob implementation

--- Modules/ShaderReflection/Include/KryneEngine/Modules/ShaderReflection/Blob.hpp
+++ Modules/ShaderReflection/Include/KryneEngine/Modules/ShaderReflection/Blob.hpp
@@ -20,14 +20,26 @@ namespace KryneEngine::Modules::ShaderReflection
      *      - header
      *      - entry point indirection table
      *      - entry point blobs
+     *      - descriptor set blobs
      *      - strings table
+     *
+     * Entry points are kept in contiguous memory, as they are expected to be accessed in sequence often, mostly by
+     * iterating over them to verify if one of them matches a given entry point name.
+     *
+     * Since entry points are kept in contiguous memory, descriptor set blobs are kept in a separate part of the file.
+     * The descriptor set blobs are regrouped by entry point for optimal cache hit with most likely access pattern.
+     * Each descriptor set header is followed by its descriptor list for the same reason. More often than not we expect
+     * to access a descriptor set in its entirety rather than iterating of the set headers.
+     *
      * Strings are kept in a separate table, as most of them may not be accessed at all, and could thus take up some
      * cache space for nothing.
      */
-    struct alignas(8) Blob
+    struct alignas(sizeof(u64)) Blob
     {
+        static constexpr size_t kAlignment = sizeof(u64);
         static constexpr u32 kMagicNumber = 0x91eb21ad; // 'keshrf' in base64
         static constexpr u32 kVersion = 0;
+        static constexpr u32 kMaxStringLength = 255;
 
         struct Header
         {
@@ -36,6 +48,42 @@ namespace KryneEngine::Modules::ShaderReflection
             u32 m_entryPointsCount;
             u32 m_stringsOffset;
         } m_header;
+
+        struct EntryPointHeader
+        {
+            u64 m_nameHash;
+
+            u32 m_nameOffset;
+            u16 m_stage;
+            u16 m_descriptorSetCount;
+
+            u64 m_pushConstantsSignatureHash;
+            u32 m_pushConstantsNameOffset;
+            u32 m_pushConstantsByteSize;
+        };
+
+        struct DescriptorSetHeader
+        {
+            u64 m_signatureHash;
+            u32 m_nameOffset;
+            u32 m_descriptorCount;
+        };
+
+        struct DescriptorData
+        {
+            u32 m_nameOffset;
+            u16 m_count;
+            u16 m_bindingIndex;
+            DescriptorBindingDesc::Type m_type;
+            TextureTypes m_textureType;
+        };
+
+        [[nodiscard]] const std::byte* GetByteBuffer() const { return reinterpret_cast<const std::byte*>(this); }
+        [[nodiscard]] std::byte* GetByteBuffer() { return reinterpret_cast<std::byte*>(this); }
+
+        [[nodiscard]] u32 GetEntryPointCount() const { return m_header.m_entryPointsCount; }
+        [[nodiscard]] u32 GetEntryPointOffset(u32 _index) const;
+        [[nodiscard]] const EntryPointHeader* GetEntryPointHeader(u32 _index) const;
 
         [[nodiscard]] static bool IsShaderReflectionBlob(const std::byte* _data);
         [[nodiscard]] static Blob* CreateBlob(
--- Modules/ShaderReflection/Src/Blob.cpp
+++ Modules/ShaderReflection/Src/Blob.cpp
@@ -4,10 +4,23 @@
  * @date 14/04/2025.
  */
 
+#include <KryneEngine/Core/Common/StringHelpers.hpp>
+
 #include "KryneEngine/Modules/ShaderReflection/Blob.hpp"
 
 namespace KryneEngine::Modules::ShaderReflection
 {
+    u32 Blob::GetEntryPointOffset(u32 _index) const
+    {
+        const u32* entryPointIndirectionTable = reinterpret_cast<const u32*>(this + 1);
+        return entryPointIndirectionTable[_index];
+    }
+
+    const Blob::EntryPointHeader* Blob::GetEntryPointHeader(u32 _index) const
+    {
+        return reinterpret_cast<const EntryPointHeader*>(GetByteBuffer() + GetEntryPointOffset(_index));
+    }
+
     bool Blob::IsShaderReflectionBlob(const std::byte* _data)
     {
         return *reinterpret_cast<const u32*>(_data) == kMagicNumber;
@@ -15,6 +28,158 @@ namespace KryneEngine::Modules::ShaderReflection
 
     Blob* Blob::CreateBlob(AllocatorInstance _allocator, eastl::span<const EntryPointInput> _entryPoints)
     {
-        return nullptr;
+        // Account for header
+        u32 estimatedPreStringTotal = sizeof(Header);
+
+        // Don't forget to count for extra u8 for string size
+        u32 stringTotal = 0;
+
+        constexpr auto getStringSize = [](const eastl::string_view& _string)
+        {
+            return eastl::min<u32>(_string.size(), kMaxStringLength) + 1u;
+        };
+
+        // Count entry point indirection table
+        estimatedPreStringTotal += Alignment::AlignUp(_entryPoints.size() * sizeof(u32), alignof(Blob));
+
+        for (const auto& entryPoint : _entryPoints)
+        {
+            // Count entry point header size
+            estimatedPreStringTotal += Alignment::AlignUp(sizeof(EntryPointHeader), alignof(Blob));
+
+            // Append entry point name
+            stringTotal += getStringSize(entryPoint.m_name);
+
+            // If applicable, save space for push constant name
+            stringTotal += entryPoint.m_pushConstants.has_value() ? getStringSize(entryPoint.m_pushConstants->m_name) : 0;
+
+            // Count descriptor set indirection table
+            estimatedPreStringTotal += Alignment::AlignUp(entryPoint.m_descriptorSets.size() * sizeof(u32), alignof(Blob));
+
+            for (const auto& descriptorSet : entryPoint.m_descriptorSets)
+            {
+                estimatedPreStringTotal += sizeof(DescriptorSetHeader);
+                stringTotal += getStringSize(descriptorSet.m_name);
+
+                estimatedPreStringTotal += Alignment::AlignUp(sizeof(DescriptorData) * descriptorSet.m_descriptors.size(), alignof(Blob));
+
+                for (const auto& descriptor : descriptorSet.m_descriptors)
+                {
+                    stringTotal += getStringSize(descriptor.m_name);
+                }
+            }
+        }
+
+        // Allocate blob with the correct size from the get-go
+        Blob* blob = reinterpret_cast<Blob*>(_allocator.allocate(estimatedPreStringTotal + stringTotal));
+
+        // Fill in the blob header
+        blob->m_header.m_magic = kMagicNumber;
+        blob->m_header.m_version = kVersion;
+        blob->m_header.m_entryPointsCount = _entryPoints.size();
+        blob->m_header.m_stringsOffset = estimatedPreStringTotal;
+
+        std::byte* dataIt = nullptr; // Will keep track of the next non-string data save location
+
+        u8* stringIt = reinterpret_cast<u8*>(blob) + estimatedPreStringTotal;
+        const auto registerName = [&](eastl::string_view _name)
+        {
+            const u8 length = eastl::min<u32>(_name.size(), kMaxStringLength);
+
+            KE_ASSERT_MSG(
+                stringIt + length + 1 - reinterpret_cast<const u8*>(blob) > (estimatedPreStringTotal + stringTotal),
+                "Out of string space!");
+
+            memcpy(stringIt + 1, _name.data(), length);
+            *stringIt = length;
+            const u32 offset = stringIt - reinterpret_cast<const u8*>(blob);
+            stringIt += length + 1;
+            return offset;
+        };
+
+        // Fill in entry points blobs first
+        {
+            u32* entryPointIndirectionIt = reinterpret_cast<u32*>(blob + 1);
+
+            dataIt = reinterpret_cast<std::byte*>(entryPointIndirectionIt) + Alignment::AlignUp(sizeof(u32) * _entryPoints.size(), alignof(Blob));
+
+            for (const auto& entryPoint: _entryPoints)
+            {
+                *entryPointIndirectionIt = dataIt - blob->GetByteBuffer();
+                entryPointIndirectionIt++;
+
+                auto* entryPointHeader = reinterpret_cast<EntryPointHeader*>(dataIt);
+                dataIt += sizeof(EntryPointHeader) + Alignment::AlignUp(sizeof(u32) * entryPoint.m_descriptorSets.size(), alignof(Blob));
+
+                entryPointHeader->m_nameHash = StringHash::Hash64(entryPoint.m_name);
+                entryPointHeader->m_nameOffset = registerName(entryPoint.m_name);
+                entryPointHeader->m_stage = static_cast<u16>(entryPoint.m_stage);
+                entryPointHeader->m_descriptorSetCount = entryPoint.m_descriptorSets.size();
+
+                if (entryPoint.m_pushConstants.has_value())
+                {
+                    entryPointHeader->m_pushConstantsSignatureHash = Hashing::Hash64Append(
+                        entryPoint.m_pushConstants->m_size,
+                        StringHash::Hash64(entryPoint.m_pushConstants->m_name));
+                    entryPointHeader->m_pushConstantsNameOffset = registerName(entryPoint.m_pushConstants->m_name);
+                    entryPointHeader->m_pushConstantsByteSize = entryPoint.m_pushConstants->m_size;
+                }
+                else
+                {
+                    entryPointHeader->m_pushConstantsSignatureHash = 0;
+                    entryPointHeader->m_pushConstantsNameOffset = 0;
+                    entryPointHeader->m_pushConstantsByteSize = 0;
+                }
+            }
+        }
+
+        // Fill in descriptor set blobs
+        for (auto entryPointIdx = 0u; entryPointIdx < blob->GetEntryPointCount(); entryPointIdx++)
+        {
+            const u32 entryPointOffset = blob->GetEntryPointOffset(entryPointIdx);
+            auto* descriptorSetIndirectionIt = reinterpret_cast<u32*>(blob->GetByteBuffer() + entryPointOffset);
+
+            for (const auto& descriptorSet : _entryPoints[entryPointIdx].m_descriptorSets)
+            {
+                *descriptorSetIndirectionIt = dataIt - blob->GetByteBuffer();
+                descriptorSetIndirectionIt++;
+
+                auto* descriptorSetHeader = reinterpret_cast<DescriptorSetHeader*>(dataIt);
+                dataIt += sizeof(DescriptorSetHeader);
+
+                descriptorSetHeader->m_signatureHash = StringHash::Hash64(descriptorSet.m_name);
+                descriptorSetHeader->m_nameOffset = registerName(descriptorSet.m_name);
+                descriptorSetHeader->m_descriptorCount = descriptorSet.m_descriptors.size();
+
+                auto* descriptorIt = reinterpret_cast<DescriptorData*>(dataIt);
+                dataIt += Alignment::AlignUp(sizeof(DescriptorData) * descriptorSet.m_descriptors.size(), alignof(Blob));
+
+                for (const auto& descriptor : descriptorSet.m_descriptors)
+                {
+                    descriptorIt->m_nameOffset = registerName(descriptor.m_name);
+
+                    descriptorSetHeader->m_signatureHash = Hashing::Hash64Append(
+                        descriptor.m_name.data(),
+                        descriptor.m_name.size(),
+                        descriptorSetHeader->m_signatureHash);
+
+                    descriptorIt->m_count = descriptor.m_count;
+                    descriptorIt->m_bindingIndex = descriptor.m_bindingIndex;
+                    descriptorIt->m_type = descriptor.m_type;
+                    descriptorIt->m_textureType = descriptor.m_textureType;
+
+                    descriptorSetHeader->m_signatureHash = Hashing::Hash64Append(
+                        reinterpret_cast<const std::byte*>(&descriptorIt->m_count),
+                        sizeof(descriptorIt->m_count) + sizeof(descriptorIt->m_bindingIndex) + sizeof(descriptorIt->m_type) + sizeof(descriptorIt->m_textureType),
+                        descriptorSetHeader->m_signatureHash);
+
+                    descriptorIt++;
+                }
+            }
+        }
+
+        KE_ASSERT_MSG(stringIt == reinterpret_cast<u8*>(blob) + estimatedPreStringTotal + stringTotal, "Unused extra padding");
+
+        return blob;
     }
 }
