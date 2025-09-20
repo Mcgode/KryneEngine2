e9dacc2354c215bc9591eca4251c8750da429273
maxgodefroy <max@godefroy.net>
2025-09-20 21:59:18 +0200

    [Core][Math] Add `constexpr` to Vector4Base constructors for improved compile-time optimization

--- Core/Include/KryneEngine/Core/Math/Vector4.hpp
+++ Core/Include/KryneEngine/Core/Math/Vector4.hpp
@@ -32,23 +32,23 @@ namespace KryneEngine::Math
             && std::is_constructible_v<T, U1>
             && std::is_constructible_v<T, U2>
             && std::is_constructible_v<T, U3>
-        Vector4Base(U0 _x, U1 _y, U2 _z = 0, U3 _w = 0) : x(_x), y(_y), z(_z), w(_w) {}
+        constexpr Vector4Base(U0 _x, U1 _y, U2 _z = 0, U3 _w = 0) : x(_x), y(_y), z(_z), w(_w) {}
 
         template <typename U>
         requires std::is_constructible_v<T, U>
-        explicit Vector4Base(U _value) : Vector4Base(_value, _value, _value, _value) {}
+        explicit constexpr Vector4Base(U _value) : Vector4Base(_value, _value, _value, _value) {}
 
         template <typename U, bool S>
         requires std::is_constructible_v<T, U>
-        explicit Vector4Base(const Vector4Base<U, S> &_other) : Vector4Base(_other.x, _other.y, _other.z, _other.w) {}
+        explicit constexpr Vector4Base(const Vector4Base<U, S> &_other) : Vector4Base(_other.x, _other.y, _other.z, _other.w) {}
 
         template <typename U0, typename U1, bool S>
         requires std::is_constructible_v<T, U0> && std::is_constructible_v<T, U1>
-        explicit Vector4Base(const Vector3Base<U0, S>& _vec3, U1 _w = 0): Vector4Base(_vec3.x, _vec3.y, _vec3.z, _w) {}
+        explicit constexpr Vector4Base(const Vector3Base<U0, S>& _vec3, U1 _w = 0): Vector4Base(_vec3.x, _vec3.y, _vec3.z, _w) {}
 
         template <typename U0, typename U1, typename U2, bool S>
         requires std::is_constructible_v<T, U0> && std::is_constructible_v<T, U1> && std::is_constructible_v<T, U2>
-        explicit Vector4Base(const Vector2Base<U0, S>& _vec2, U1 _z = 0, U2 _w = 0): Vector4Base(_vec2.x, _vec2.y, _z, _w) {}
+        explicit constexpr Vector4Base(const Vector2Base<U0, S>& _vec2, U1 _z = 0, U2 _w = 0): Vector4Base(_vec2.x, _vec2.y, _z, _w) {}
 
         Vector4Base operator+(const Vector4Base& _other) const;
         Vector4Base operator-(const Vector4Base& _other) const;
