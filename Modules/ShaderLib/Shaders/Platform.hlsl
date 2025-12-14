55a43a276eb5f573ef889d07e52e6530c8d26d6a
maxgodefroy <max@godefroy.net>
2025-12-14 20:23:29 +0100

    [Modules][ShaderLib] Add utility functions for unpacking packed data
    Introduced `unpackHalf2x16ToFloat` and `unpackUnorm4x8ToFloat` functions in HLSL to decode packed data into expanded floating-point representations.

--- Modules/ShaderLib/Shaders/Platform.hlsl
+++ Modules/ShaderLib/Shaders/Platform.hlsl
@@ -2,3 +2,13 @@
 #define vkPushConstant [[vk::push_constant]]
 #define vkLocation(index) [[vk::location(index)]]
 #define vkBinding(index, set) [[vk::binding(index, set)]]
+
+float2 unpackHalf2x16ToFloat(in uint _packedHalf2)
+{
+    return f16tof32(uint2(_packedHalf2 & 0xffff, _packedHalf2 >> 16));
+}
+
+float4 unpackUnorm4x8ToFloat(uint _packedUnorm)
+{
+    return float4(unpack_u8u32(_packedUnorm)) / 255.0f;
+}
