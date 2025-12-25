31888b3ff762518646aefc2f39f9e9a7f91e9abf
maxgodefroy <max@godefroy.net>
2025-12-25 17:19:41 +0100

    [Modules][GuiLib] Add support for independent corner radii in `BasicGuiRenderer`
    Updated shader logic to enable independent corner radius configurations for rounded boxes. Refactored related structure and SDF calculations to accommodate per-corner radii, improving UI design flexibility.

--- Modules/GuiLib/Shaders/BasicGuiRenderer/Rectangle.hlsl
+++ Modules/GuiLib/Shaders/BasicGuiRenderer/Rectangle.hlsl
@@ -12,7 +12,7 @@ struct VsOutput
 {
     float4 position : SV_POSITION;
     float2 pixelPosition : POSITION0;
-    nointerpolation float cornerRadius : TEXCOORD0;
+    nointerpolation uint2 cornerRadii : TEXCOORD0;
     nointerpolation uint backgroundColor : COLOR;
     nointerpolation float2 rectHalfSize: POSITION1;
 };
@@ -28,7 +28,7 @@ VsOutput RectangleVs(VsInput _input)
     const float2 rectHalfSize = unpackHalf2x16ToFloat(_input.packedRect.y);
 
     output.backgroundColor = _input.packedData.z;
-    output.cornerRadius = unpackHalf2x16ToFloat(_input.packedData.x).x;
+    output.cornerRadii = _input.packedData.xy;
     output.rectHalfSize = rectHalfSize;
 
     const float2 baseVertexPos = vertices[_input.vertexId];
@@ -43,21 +43,26 @@ VsOutput RectangleVs(VsInput _input)
 }
 
 // Rounded box SDF calculations based on https://www.shadertoy.com/view/4cG3R1
-float SDRoundedBox(float2 _position, float2 _halfSize, float _cornerRadius)
+float SDRoundedBox(const float2 _position, const float2 _halfSize, float4 _cornerRadii)
 {
-    const float2 q = abs(_position) - _halfSize + _cornerRadius;
+    _cornerRadii.xy = _position.y > 0.f ? _cornerRadii.xy : _cornerRadii.zw;
+    const float cornerRadius = _position.x > 0.f ? _cornerRadii.x : _cornerRadii.y;
+    const float2 q = abs(_position) - _halfSize + cornerRadius;
 
     if (min(q.x, q.y) < 0.f)
-        return max(q.x, q.y) - _cornerRadius;
+        return max(q.x, q.y) - cornerRadius;
 
-    const float2 uv = float2(abs(q.x - q.y), q.x + q.y - _cornerRadius) / _cornerRadius;
-    return (length(uv - float2(0, -1)) - sqrt(2.f)) * _cornerRadius * sqrt(0.5f);
+    const float2 uv = float2(abs(q.x - q.y), q.x + q.y - cornerRadius) / cornerRadius;
+    return (length(uv - float2(0, -1)) - sqrt(2.f)) * cornerRadius * sqrt(0.5f);
 }
 
 [shader("pixel")]
 float4 RectangleFs(FsInput _input): SV_TARGET0
 {
-    const float d = SDRoundedBox(_input.pixelPosition, _input.rectHalfSize, _input.cornerRadius);
+    const float d = SDRoundedBox(
+        _input.pixelPosition,
+        _input.rectHalfSize,
+        float4(unpackHalf2x16ToFloat(_input.cornerRadii.x), unpackHalf2x16ToFloat(_input.cornerRadii.y)));
 
     const float2 dPixelPos = ddx(_input.pixelPosition);
     const float scale = length(dPixelPos) * 0.5;
