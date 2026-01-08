24909911ce619612b43032ff4fb8ae78e3ca1d73
maxgodefroy <max@godefroy.net>
2026-01-08 13:40:20 +0100

    [Core][Memory] Rename and enhance VectorLinkedList to VectorDeLinkedList
    Refactored `VectorLinkedList` into `VectorDeLinkedList` to support double-ended functionality with added `m_previous` links. Adjusted constant identifiers and allocation/freelist logic. Updated CMakeLists accordingly.

--- Core/Include/KryneEngine/Core/Memory/Containers/VectorDeLinkedList.hpp
+++ Core/Include/KryneEngine/Core/Memory/Containers/VectorDeLinkedList.hpp
@@ -8,6 +8,7 @@
 
 #include <EASTL/vector.h>
 
+#include "KryneEngine/Core/Common/Assert.hpp"
 #include "KryneEngine/Core/Common/Types.hpp"
 #include "KryneEngine/Core/Memory/Allocators/Allocator.hpp"
 
@@ -15,7 +16,7 @@ namespace KryneEngine
 {
 
     /**
-     * @brief A templated class representing a linked list that uses an internal vector for storage.
+     * @brief A templated class representing a double-ended linked list that uses an internal vector for storage.
      * The class supports both intrusive and non-intrusive types for nodes.
      *
      * @tparam T The type of elements stored in the list. For intrusive nodes, it must have a `m_next` u32 member.
@@ -27,19 +28,19 @@ namespace KryneEngine
      * flexible usage and customization.
      * The indices are stable over time. When freeing an index, it is added to the freelist for reuse.
      */
-    template <class T, bool Intrusive = !std::is_same_v<u32, decltype(T::m_next)>>
-    class VectorLinkedList
+    template <class T, bool Intrusive = std::is_same_v<u32, decltype(T::m_next)> && std::is_same_v<u32, decltype(T::m_previous)>>
+    class VectorDeLinkedList
     {
     public:
-        explicit VectorLinkedList(AllocatorInstance _allocator): m_vector(_allocator) {}
+        explicit VectorDeLinkedList(AllocatorInstance _allocator): m_vector(_allocator) {}
 
-        VectorLinkedList(const VectorLinkedList&) = default;
-        VectorLinkedList(VectorLinkedList&&) = default;
-        VectorLinkedList& operator=(const VectorLinkedList&) = default;
-        VectorLinkedList& operator=(VectorLinkedList&&) = default;
+        VectorDeLinkedList(const VectorDeLinkedList&) = default;
+        VectorDeLinkedList(VectorDeLinkedList&&) = default;
+        VectorDeLinkedList& operator=(const VectorDeLinkedList&) = default;
+        VectorDeLinkedList& operator=(VectorDeLinkedList&&) = default;
 
-        /// Special constant value used to indicate the end of a linked list.
-        static constexpr u32 kListEndId = ~0u;
+        /// Special constant value used to indicate the beginning or the end of a linked list.
+        static constexpr u32 kListLimitId = ~0u;
 
         T& operator[](u32 _index)
         {
@@ -59,17 +60,20 @@ namespace KryneEngine
 
         u32 AllocateNode()
         {
-            if (m_firstFree == kListEndId)
+            if (m_firstFree == kListLimitId)
             {
                 m_vector.emplace_back();
-                m_vector.back().m_next = kListEndId;
+                m_vector.back().m_next = kListLimitId;
+                m_vector.back().m_previous = kListLimitId;
                 return m_vector.size() - 1;
             }
             else
             {
                 const u32 result = m_firstFree;
                 m_firstFree = m_vector[result].m_next;
-                m_vector[result].m_next = kListEndId;
+                KE_ASSERT(m_vector[result].m_previous == kListLimitId);
+                m_vector[result].m_previous = kListLimitId;
+                m_vector[result].m_next = kListLimitId;
                 return result;
             }
         }
@@ -84,14 +88,24 @@ namespace KryneEngine
             return m_vector[_index].m_next;
         }
 
+        u32 GetPrevious(u32 _index) const
+        {
+            return m_vector[_index].m_previous;
+        }
+
         void SetNext(u32 _index, u32 _nextIndex) const
         {
             m_vector[_index].m_next = _nextIndex;
+            m_vector[_nextIndex].m_previous = _index;
         }
 
         void FreeNode(u32 _index)
         {
-            m_vector[_index].m_next = m_firstFree;
+            Node& node = m_vector[_index];
+            if (node.m_previous != kListLimitId)
+                m_vector[node.m_previous].m_next = node.m_next;
+            node.m_previous = kListLimitId;
+            node.m_next = m_firstFree;
             m_firstFree = _index;
         }
 
@@ -100,11 +114,12 @@ namespace KryneEngine
         {
             T m_value;
             u32 m_next;
+            u32 m_previous;
         };
 
         using Node = std::conditional_t<Intrusive, T, NonIntrusiveNode>;
 
         eastl::vector<Node> m_vector;
-        u32 m_firstFree = kListEndId;
+        u32 m_firstFree = kListLimitId;
     };
 }
