0ccb9556a06ec569aae0d353f674e13073233cf9
maxgodefroy <max@godefroy.net>
2026-01-13 14:06:44 +0100

    [Core][Platform] Add font glyph retrieval for system default fonts on Darwin
    Implemented `RetrieveSystemDefaultGlyph` to extract glyph outlines and metrics from the system font on Darwin platforms, enabling improved text rendering functionality. Added platform-specific build setup for the new feature.

---
+++ Core/Include/KryneEngine/Core/Platform/Platform.hpp
@@ -0,0 +1,65 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 12/01/2026.
+ */
+
+#pragma once
+
+#include <EASTL/span.h>
+
+#include "KryneEngine/Core/Common/Types.hpp"
+#include "KryneEngine/Core/Math/Vector.hpp"
+#include "KryneEngine/Core/Memory/Allocators/Allocator.hpp"
+
+namespace KryneEngine::Platform
+{
+
+    /**
+     * @defgroup Font methods
+     * @{
+     */
+
+    struct FontMetrics
+    {
+        double m_ascender;
+        double m_descender;
+        double m_lineHeight;
+    };
+
+    struct GlyphMetrics
+    {
+        double m_advance;
+        double m_width;
+        double m_height;
+        double m_bearingX;
+        double m_bearingY;
+    };
+
+    using FontGlyphMetricsFunction = void (*)(const FontMetrics&, const GlyphMetrics&, void*);
+    using FontNewContourFunction = void (*)(const double2&, void*);
+    using FontNewEdgeFunction = void (*)(const double2&, void*);
+    using FontNewConicFunction = void (*)(const double2&, const double2&, void*);
+    using FontNewCubicFunction = void (*)(const double2&, const double2&, const double2&, void*);
+    using FontEndContourFunction = void (*)(void*);
+
+    /**
+     * @brief A function that retrieves the outline of a glyph from the system default font.
+     *
+     * @returns `true` if the glyph was successfully retrieved, `false` otherwise.
+     */
+    bool RetrieveSystemDefaultGlyph(
+        u32 _unicodeCodePoint,
+        void* _userData,
+        FontGlyphMetricsFunction _fontMetrics,
+        FontNewContourFunction _newContour,
+        FontNewEdgeFunction _newEdge,
+        FontNewConicFunction _newConic,
+        FontNewCubicFunction _newCubic,
+        FontEndContourFunction _endContour,
+        bool _verticalLayout = false);
+
+    /**
+     * @}
+     */
+}
---
+++ Core/Src/Platform/Darwin/Font.cpp
@@ -0,0 +1,148 @@
+/**
+ * @file
+ * @author Max Godefroy
+ * @date 13/01/2026.
+ */
+
+#include "KryneEngine/Core/Platform/Platform.hpp"
+
+#include <CoreText/CoreText.h>
+#include <EASTL/fixed_vector.h>
+
+namespace KryneEngine::Platform
+{
+    bool RetrieveSystemDefaultGlyph(
+        u32 _unicodeCodePoint,
+        void* _userData,
+        FontGlyphMetricsFunction _fontMetrics,
+        FontNewContourFunction _newContour,
+        FontNewEdgeFunction _newEdge,
+        FontNewConicFunction _newConic,
+        FontNewCubicFunction _newCubic,
+        FontEndContourFunction _endContour,
+        bool _verticalLayout)
+    {
+        eastl::fixed_vector<UniChar, 2, false> utf16;
+        if (_unicodeCodePoint < 0xD800 || (_unicodeCodePoint > 0xDFFF && _unicodeCodePoint < 0x10000))
+        {
+            utf16.push_back(static_cast<UniChar>(_unicodeCodePoint));
+        }
+        else
+        {
+            const u32 cp = _unicodeCodePoint - 0x10000;
+            utf16.push_back(static_cast<UniChar>(0xD800 + (cp >> 10)));
+            utf16.push_back(static_cast<UniChar>(0xDC00 + (cp & 0x3FF)));
+        }
+
+        const CTFontRef defaultFont = CTFontCreateUIFontForLanguage(kCTFontUIFontSystem, 16.f, nullptr);
+        const CFArrayRef cascadeDescriptors = CTFontCopyDefaultCascadeListForLanguages(defaultFont, nullptr);
+        if (cascadeDescriptors == nullptr) return false;
+
+        bool found = false;
+        const CFIndex cascadeCount = CFArrayGetCount(cascadeDescriptors);
+        for (CFIndex i = 0; i < cascadeCount; ++i)
+        {
+            auto descriptor = static_cast<CTFontDescriptorRef>(CFArrayGetValueAtIndex(cascadeDescriptors, i));
+            if (descriptor == nullptr)
+                continue;
+
+            const CTFontRef font = CTFontCreateWithFontDescriptor(descriptor, 16.f, nullptr);
+            if (font == nullptr) continue;
+
+            eastl::fixed_vector<CGGlyph, 2, false> glyphs(utf16.size());
+            const bool ok = CTFontGetGlyphsForCharacters(font, utf16.data(), glyphs.data(), glyphs.size());
+
+            if (ok && glyphs.front() != 0)
+            {
+                found = true;
+
+                CGPathRef outline = CTFontCreatePathForGlyph(font, glyphs.front(), nullptr);
+                if (outline != nullptr)
+                {
+                    KE_ASSERT_MSG(!_verticalLayout, "Vertical layout not implemented");
+
+                    const FontMetrics fontMetrics {
+                        .m_ascender = CTFontGetAscent(font),
+                        .m_descender = CTFontGetDescent(font),
+                        .m_lineHeight = CTFontGetLeading(font) + CTFontGetAscent(font) + CTFontGetDescent(font),
+                    };
+
+                    CGSize advances;
+                    CTFontGetAdvancesForGlyphs(font, kCTFontOrientationHorizontal, glyphs.data(), &advances, 1);
+
+                    CGRect bbox;
+                    CTFontGetBoundingRectsForGlyphs(font, kCTFontOrientationHorizontal, glyphs.data(), &bbox, 1);
+
+                    const GlyphMetrics glyphMetrics {
+                        .m_advance = advances.width,
+                        .m_width = bbox.size.width,
+                        .m_height = bbox.size.height,
+                        .m_bearingX = bbox.origin.x,
+                        .m_bearingY = bbox.size.height + bbox.origin.y,
+                    };
+
+                    _fontMetrics(fontMetrics, glyphMetrics, _userData);
+
+                    struct Context
+                    {
+                        void* m_userData;
+                        FontNewContourFunction m_newContour;
+                        FontNewEdgeFunction m_newEdge;
+                        FontNewConicFunction m_newConic;
+                        FontNewCubicFunction m_newCubic;
+                        FontEndContourFunction m_endContour;
+                        CGPoint m_firstPoint {};
+                    };
+                    Context context { _userData, _newContour, _newEdge, _newConic, _newCubic, _endContour };
+                    constexpr auto applier = [](void* _info, const CGPathElement* _element)
+                    {
+                        auto& context = *static_cast<Context*>(_info);
+
+                        switch (_element->type)
+                        {
+
+                        case kCGPathElementMoveToPoint:
+                            context.m_firstPoint = _element->points[0];
+                            context.m_newContour(
+                                { _element->points[0].x, _element->points[0].y },
+                                context.m_userData);
+                            break;
+                        case kCGPathElementAddLineToPoint:
+                            context.m_newEdge(
+                                { _element->points[0].x, _element->points[0].y },
+                                context.m_userData);
+                            break;
+                        case kCGPathElementAddQuadCurveToPoint:
+                            context.m_newConic(
+                                { _element->points[0].x, _element->points[0].y },
+                                { _element->points[1].x, _element->points[1].y },
+                                context.m_userData);
+                            break;
+                        case kCGPathElementAddCurveToPoint:
+                            context.m_newCubic(
+                                { _element->points[0].x, _element->points[0].y },
+                                { _element->points[1].x, _element->points[1].y },
+                                { _element->points[2].x, _element->points[2].y },
+                                context.m_userData);
+                            break;
+                        case kCGPathElementCloseSubpath:
+                            context.m_endContour(context.m_userData);
+                            break;
+                        }
+                    };
+
+                    CGPathApply(outline, &context, applier);
+                }
+
+                CFRelease(font);
+                break;
+            }
+
+            CFRelease(font);
+        }
+
+        CFRelease(cascadeDescriptors);
+        CFRelease(defaultFont);
+        return found;
+    }
+}
