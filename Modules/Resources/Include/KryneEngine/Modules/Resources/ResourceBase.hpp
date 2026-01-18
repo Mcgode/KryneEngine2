d721a7c2807815cc5445dcad09334ac1c2bedd62
maxgodefroy <max@godefroy.net>
2026-01-18 13:23:47 +0100

    [Modules][Resources] Add version tracking to `ResourceBase` and derived classes
    Updated `ResourceBase` and `RefCountedResourceBase` to include version tracking functionality. Added `GetVersion` method and modified constructors to accept a version parameter.

--- Modules/Resources/Include/KryneEngine/Modules/Resources/ResourceBase.hpp
+++ Modules/Resources/Include/KryneEngine/Modules/Resources/ResourceBase.hpp
@@ -15,24 +15,34 @@ namespace KryneEngine::Modules::Resources
     template <class ResourceManager>
     class ResourceBase
     {
+        friend ResourceManager;
+
     public:
         [[nodiscard]] AllocatorInstance GetAllocator() const { return m_allocator; }
+
+        [[nodiscard]] ResourceManager* GetResourceManager() const { return m_resourceManager; }
+
+        [[nodiscard]] u64 GetVersion() const { return m_version; }
 
         virtual ~ResourceBase() = default;
 
     protected:
         AllocatorInstance m_allocator;
         ResourceManager* m_resourceManager;
+        size_t m_version;
 
-        ResourceBase(AllocatorInstance _allocator, ResourceManager* _resourceManager)
+        ResourceBase(AllocatorInstance _allocator, ResourceManager* _resourceManager, size_t _version)
             : m_allocator(_allocator)
             , m_resourceManager(_resourceManager)
+            , m_version(_version)
         {}
     };
 
     template <class ResourceManager>
     class RefCountedResourceBase: public ResourceBase<ResourceManager>
     {
+        friend ResourceManager;
+
     public:
         s32 m_refCount = 0;
 
@@ -44,9 +54,13 @@ namespace KryneEngine::Modules::Resources
     protected:
         ResourceEntry* m_entry;
 
-        RefCountedResourceBase(AllocatorInstance _allocator, ResourceManager* _resourceManager, ResourceEntry* _entry)
-            : ResourceBase<ResourceManager>(_allocator, _resourceManager)
-            , m_entry(_entry)
+        RefCountedResourceBase(
+            AllocatorInstance _allocator,
+            ResourceManager* _resourceManager,
+            ResourceEntry* _entry,
+            size_t _version)
+                : ResourceBase<ResourceManager>(_allocator, _resourceManager, _version)
+                , m_entry(_entry)
         {}
     };
 }
